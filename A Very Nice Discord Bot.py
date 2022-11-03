2d42e6de7418a7ef70454dacc50c8c4457338313
pellpk <117397809+pellpk@users.noreply.github.com>
2022-11-03 20:32:45 +0000

    Update A Very Nice Discord Bot.py

--- A Very Nice Discord Bot.py
+++ A Very Nice Discord Bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 
-TOKEN = 'MTAzMTc2NDM4NTc4MzM2NTY3Mw.G4zxNs.13YGjbiEHrrpEJaulVXXNDSrpRBC3GOKYPbH_k'
+TOKEN = 'TOKEN HERE'
 
 intents = discord.Intents.default()
 intents.message_content = True
